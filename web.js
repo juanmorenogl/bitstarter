28f440bf67993914a7fa99bea30379eab77783af
juanmorenogl <juanmorenogl@gmail.com>
2013-07-09 19:53:46 +0200

    web.js

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var fs = require('fs');
 var outfile = "index.html";
-
+var buf = fs.readFileSync(outfile);
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 //  response.send('Hello World!');
-response.send(fs.readFileSync(outfile));
+//response.send(fs.readFileSync(outfile));
+
+var xx = buf.toString('utf8', 0, buf.length);
+response.send(xx));
 });
 
 
