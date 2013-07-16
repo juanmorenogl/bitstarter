87b004c2bf5f0e28d58f64eb324fee5b425e53bd
juamorenogl <juanmorenogl@gmail.com>
2013-07-16 20:42:19 +0200

    HW3.1

--- web.js
+++ web.js
@@ -1,10 +1,25 @@
 var express = require('express');
-
+var fs = require('fs');
+var outfile = "index.html";
+//var buf = fs.readFileSync(outfile);
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World!');
+//response.send(fs.readFileSync(outfile));
+
+fs.readFileSync('/etc/passwd', function (err, data) {
+  if (err) throw err;
+//  console.log(data);
+response.send(data);
 });
+
+
+//var xx = buf.toString('utf8', 0, buf.length);
+//response.send(xx));
+//});
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
