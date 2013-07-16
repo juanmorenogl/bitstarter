2ae8bb7edb856c2ad8f722ce31b27c56814dc958
juanmorenogl <juanmorenogl@gmail.com>
2013-07-16 20:25:47 +0200

    added web.js - HelloWorld2

--- web.js
+++ web.js
@@ -1,18 +1,10 @@
 var express = require('express');
-var fs = require('fs');
-var outfile = "index.html";
-var buf = fs.readFileSync(outfile);
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-//  response.send('Hello World!');
-//response.send(fs.readFileSync(outfile));
-
-var xx = buf.toString('utf8', 0, buf.length);
-response.send(xx));
+  response.send('Hello World 2!');
 });
-
-
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
