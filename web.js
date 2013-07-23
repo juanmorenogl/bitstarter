261ac0ecc8b87b5f0318438b090f3a7b7f4ec513
juamorenogl <juanmorenogl@gmail.com>
2013-07-23 20:17:06 +0200

    web.js port 8080 ok

--- web.js
+++ web.js
@@ -1,18 +1,17 @@
+
 var express = require('express');
 var fs = require('fs');
 var outfile = "index.html";
+
 var app = express.createServer(express.logger());
 
-var texto = fs.readFileSync(outfile); 
-buff = new Buffer(texto);
-
 app.get('/', function(request, response) {
-//  response.send('Hello World!');
-//response.send(fs.readFileSync(outfile));
-	response.send(buff.toString('utf8', 0, buff.length));
+	var html = fs.readFileSync(outfile).toString();
+	response.send(html);
 });
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
